f5fc693cd326613b5635bf3b2fc7e1b84fbc46c1
Andrew <andrewdebyl@gmail.com>
2025-02-13 13:34:29 -0500

    initial

---
+++ lab2_template/settings.py
@@ -0,0 +1,17 @@
+# PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'FdrGIB3k8WY76HdkRU2ZdWDh',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'crud',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
